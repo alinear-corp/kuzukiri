ae9311c35bcc5d4031ccc7b338984f33a71aef1b
Jun-ya NORIMATSU <dummy@jnory.com>
2021-12-25 12:55:47 +0900

    v0.1

--- kuzukiri/__init__.py
+++ kuzukiri/__init__.py
@@ -1,1 +1,7 @@
 from .kuzukiri import *  # noqa
+
+__copyright__ = 'Copyright (C) ALINEAR Corporation'
+__version__ = '0.1.0'
+__license__ = 'MIT'
+__author__ = "ALINEAR Corporation"
+__url__ = "https://github.com/alinear-corp/kuzukiri"
--- setup.py
+++ setup.py
@@ -4,8 +4,26 @@ from setuptools_rust import Binding, RustExtension
 # reference: https://github.com/PyO3/setuptools-rust#setup
 setup(
     name="kuzukiri",
-    version="0.0.1",
+    version="0.1.0",
     rust_extensions=[RustExtension("kuzukiri.kuzukiri", binding=Binding.PyO3)],
     packages=["kuzukiri"],
     zip_safe=False,
+    classfiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: Japanese",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Rust",
+        "Topic :: Software Development :: Pre-processors",
+        "Topic :: Text Processing",
+    ],
+    keywords=[
+        "NLP", "Natural Language Processing",
+        "Text Segmentation", "Python", "Rust",
+        "Japanese", "Preprocessing",
+        "Text Processing",
+    ],
+    platforms=["Windows", "Linux"],
 )
