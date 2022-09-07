6a290f237935266e8d0c8ede1c844df7dae9f591
Jun-ya NORIMATSU <dummy@jnory.com>
2022-09-07 21:39:54 +0900

    Change version.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools_rust import Binding, RustExtension
 # reference: https://github.com/PyO3/setuptools-rust#setup
 setup(
     name="kuzukiri",
-    version="0.1.2",
+    version="0.1.3",
     rust_extensions=[RustExtension("kuzukiri.kuzukiri", binding=Binding.PyO3)],
     packages=["kuzukiri"],
     zip_safe=False,
